e30bcb0033bad1f139d6f0d0b469b5e2863ce0f4
Brian O'Connor <briandoconnor@gmail.com>
2013-09-14 15:45:57 -0400

    Still not sure what's going on with the null featureset

--- seqware-queryengine/src/main/java/com/github/seqware/queryengine/impl/HBaseStorage.java
+++ seqware-queryengine/src/main/java/com/github/seqware/queryengine/impl/HBaseStorage.java
@@ -780,7 +780,7 @@ public class HBaseStorage extends StorageInterface {
       // so this is Map<"featureset123", Map<timestamp_long, "featurelist">>
       // go through and convert to Map<FeatureSet, List<FeatureList>>
       for(byte[] familyQual : familyMap.keySet()) {
-        FeatureSet fs = serializer.deserialize(familyQual, FeatureSet.class);
+        LazyFeatureSet fs = serializer.deserialize(familyQual, LazyFeatureSet.class);
         System.out.println("FEATURE SET: "+fs+" RAW: "+familyQual);
         NavigableMap<Long, byte[]> familyQualTimestampMap = familyMap.get(familyQual);
         long time = 0;
