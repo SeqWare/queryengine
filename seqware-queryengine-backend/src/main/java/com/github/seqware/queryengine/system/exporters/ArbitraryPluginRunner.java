f8f022b58b0b41c549f60160b59edfe612eb00c0
bcso <bcso@uwaterloo.ca>
2014-03-03 16:08:26 -0500

    fixed cli options

--- seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/system/exporters/ArbitraryPluginRunner.java
+++ seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/system/exporters/ArbitraryPluginRunner.java
@@ -1,14 +1,27 @@
 package com.github.seqware.queryengine.system.exporters;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 
 import com.github.seqware.queryengine.model.Reference;
 import com.github.seqware.queryengine.plugins.PluginInterface;
 import com.github.seqware.queryengine.plugins.contribs.MutationsToDonorsAggregationPlugin;
 import com.github.seqware.queryengine.system.Utility;
+import com.github.seqware.queryengine.system.importers.FeatureImporter;
+import com.github.seqware.queryengine.system.importers.SOFeatureImporter;
+import com.github.seqware.queryengine.util.SGID;
 import com.github.seqware.queryengine.factory.SWQEFactory;
 
 public class ArbitraryPluginRunner {
@@ -25,58 +38,67 @@ public class ArbitraryPluginRunner {
 	private String[] args;
 	
 	public static void main(String[] args) {
-		ArbitraryPluginRunner dump = new ArbitraryPluginRunner(args);
-		dump.export();
-	}
-	
-	public void export(){
-		if (args.length < 3){
-			System.err.println(args.length + " arguments found.");
-			System.out.println("VCFDumper <reference name> <class name> <outputfile>");
-			System.exit(-1);
-		}
-		
-		String referenceName = cmd.getOptionValue(REFERENCE_ID_PARAM);
-		String plugin = cmd.getOptionValue(REFERENCE_ID_PARAM);
-		String outputFile = cmd.getOptionValue(OUTPUT_FILE_PARAM);
-
-		Reference ref = null;
-		for (Reference r : SWQEFactory.getQueryInterface().getReferences()){
-			if (referenceName.equals(r.getName())){
-				ref = r;
-				break;
-			}
-		}
-
-		Class<? extends PluginInterface> arbitraryPluginClass;
-		try {
-			arbitraryPluginClass = (Class<? extends PluginInterface>) Class.forName(plugin);
-	        long start = new Date().getTime();
-			System.out.println("Running plugin: " + plugin);
-			Utility.dumpFromMapReducePlugin(plugin, ref, null, arbitraryPluginClass, (args.length == 3 ? outputFile : null));
-	        long stop = new Date().getTime();
-	        float diff = ((stop - start) / 1000) / 60;
-	        System.out.println("Minutes to query: "+diff);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		if (ref == null){
-			System.out.println("Reference was not found.");
-			System.exit(-2);
-		}
+		SGID mainMethod = ArbitraryPluginRunner.runArbitraryPluginRunner(args);
+        if (mainMethod == null) {
+            System.exit(FeatureImporter.EXIT_CODE_INVALID_FILE);
+        }
 	}
 
-	public ArbitraryPluginRunner(String[] args){
-		this.args = args;
-		Options option = new Options();
-		Option option1 = OptionBuilder.withArgName("outputFile").withDescription("(required output file").hasArgs(1).isRequired.create(OUTPUT_FILE_PARAM);
+	public static SGID runArbitraryPluginRunner(String[] args){
+
+		Options options = new Options();
+		Option option1 = OptionBuilder.withArgName("outputFile").withDescription("(required output file").hasArgs(1).isRequired().create(OUTPUT_FILE_PARAM);
 		options.addOption(option1);
-		Option option2 = OptionBuilder.withArgName("reference").withDescription("(required) the reference ID of the FeatureSet to run plugin on").hasArgs(1).isRequired.create(REFERENCE_ID_PARAM);
+		Option option2 = OptionBuilder.withArgName("reference").withDescription("(required) the reference ID of the FeatureSet to run plugin on").hasArgs(1).isRequired().create(REFERENCE_ID_PARAM);
 		options.addOption(option2);
-		Option option3 = OptionBuilder.withArgName("pluginClass").withDescription("(required) the plugin to be run, full package path");
+		Option option3 = OptionBuilder.withArgName("pluginClass").withDescription("(required) the plugin to be run, full package path").hasArgs(1).isRequired().create(PLUGIN_CLASS_PARAM);
 		options.addOption(option3);
+		
+		try{			
+			CommandLineParser parser = new PosixParser();
+			CommandLine cmd = parser.parse(options, args);
+			String referenceName = cmd.getOptionValue(REFERENCE_ID_PARAM);
+			String plugin = cmd.getOptionValue(REFERENCE_ID_PARAM);
+			String outputFile = cmd.getOptionValue(OUTPUT_FILE_PARAM);
 
+			Reference ref = null;
+			for (Reference r : SWQEFactory.getQueryInterface().getReferences()){
+				if (referenceName.equals(r.getName())){
+					ref = r;
+					break;
+				}
+			}
+
+			Class<? extends PluginInterface> arbitraryPluginClass;
+			try {
+
+				arbitraryPluginClass = (Class<? extends PluginInterface>) Class.forName(plugin);
+		        long start = new Date().getTime();
+				System.out.println("Running plugin: " + plugin);
+				Utility.dumpFromMapReducePlugin(plugin, ref, null, arbitraryPluginClass, (args.length == 3 ? outputFile : null));
+		        long stop = new Date().getTime();
+		        float diff = ((stop - start) / 1000) / 60;
+		        System.out.println("Minutes to query: "+diff);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if (ref == null){
+				System.out.println("Reference was not found.");
+				System.exit(-2);
+			}
+		} catch (MissingOptionException e) {
+            // automatically generate the help statement
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(SOFeatureImporter.class.getSimpleName(), options);
+            System.exit(FeatureImporter.EXIT_CODE_INVALID_ARGS);
+        } catch (ParseException e) {
+            // automatically generate the help statement
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(SOFeatureImporter.class.getSimpleName(), options);
+            System.exit(FeatureImporter.EXIT_CODE_INVALID_ARGS);
+        }
+        return null;
 	}
 }
