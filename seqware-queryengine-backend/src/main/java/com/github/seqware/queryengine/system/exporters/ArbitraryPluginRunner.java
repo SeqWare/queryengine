2b7c8a1af6051c6eae8d14b89a52366395126b77
bcso <bcso@uwaterloo.ca>
2014-03-03 17:33:26 -0500

    fix error output

--- seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/system/exporters/ArbitraryPluginRunner.java
+++ seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/system/exporters/ArbitraryPluginRunner.java
@@ -83,12 +83,12 @@ public class ArbitraryPluginRunner {
 		} catch (MissingOptionException e) {
             // automatically generate the help statement
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp(SOFeatureImporter.class.getSimpleName(), options);
+            formatter.printHelp("ArbitraryPluginRunner usage:", options);
             System.exit(FeatureImporter.EXIT_CODE_INVALID_ARGS);
         } catch (ParseException e) {
             // automatically generate the help statement
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp(SOFeatureImporter.class.getSimpleName(), options);
+            formatter.printHelp("ArbitraryPluginRunner usage:", options);
             System.exit(FeatureImporter.EXIT_CODE_INVALID_ARGS);
         } catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
