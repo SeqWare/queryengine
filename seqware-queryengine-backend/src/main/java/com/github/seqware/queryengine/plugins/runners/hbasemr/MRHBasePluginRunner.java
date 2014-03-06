367856f96710311374607b8a5946d59d7f871001
bcso <bcso@uwaterloo.ca>
2014-03-06 17:48:48 -0500

    get name of dumper plugin being run

--- seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/plugins/runners/hbasemr/MRHBasePluginRunner.java
+++ seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/plugins/runners/hbasemr/MRHBasePluginRunner.java
@@ -326,10 +326,10 @@ public final class MRHBasePluginRunner<ReturnType> implements PluginRunnerInterf
             scan.setCacheBlocks(false);  // don't set to true for MR jobs
             
             //Apply scan filter only for non write plugin
-            if (mapReducePlugin.getClass().getSimpleName().equals("VCFDumperPlugin")){
-                scan.setFilter(finalFilterList);
-            }
-            
+//            if (mapReducePlugin.getClass().getSimpleName().equals("VCFDumperPlugin")){
+//                scan.setFilter(finalFilterList);
+//            }
+            System.out.println("SIMPLE NAME: " + mapReducePlugin.getClass().getSimpleName());
             for(FeatureSet set : inputSet){
                 byte[] qualiferBytes = Bytes.toBytes(set.getSGID().getUuid().toString());
                 scan.addColumn(HBaseStorage.getTEST_FAMILY_INBYTES(), qualiferBytes);
