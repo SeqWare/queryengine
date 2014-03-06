9aa003f557efb17b60e183112e6c3e14291da0ed
bcso <bcso@uwaterloo.ca>
2014-03-06 13:44:30 -0500

    see if regexp works

--- seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/plugins/runners/hbasemr/MRHBasePluginRunner.java
+++ seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/plugins/runners/hbasemr/MRHBasePluginRunner.java
@@ -254,7 +254,7 @@ public final class MRHBasePluginRunner<ReturnType> implements PluginRunnerInterf
     		}
             
     		
-           Filter rowFilter = new RowFilter(CompareFilter.CompareOp.GREATER, new RegexStringComparator("hg_19smallTestNoOverlap.1:000000000013200"));
+           Filter rowFilter = new RowFilter(CompareFilter.CompareOp.EQUAL, new RegexStringComparator("hg_19smallTestNoOverlap.1:000000000013200"));
 
             Scan scan = new Scan();
             scan.setMaxVersions();       // we need all version data
