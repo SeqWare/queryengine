08851ceefd89345382cf13527ec5c149c340a6ac
bcso <bcso@uwaterloo.ca>
2014-03-17 12:11:06 -0400

    add all cases

--- seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/plugins/runners/hbasemr/MRHBasePluginRunner.java
+++ seqware-queryengine-backend/src/main/java/com/github/seqware/queryengine/plugins/runners/hbasemr/MRHBasePluginRunner.java
@@ -135,7 +135,8 @@ public final class MRHBasePluginRunner<ReturnType> implements PluginRunnerInterf
     private static boolean START_STOP_PAIRS_EXIST = false;
     private static List<FeatureSet> thisInputSet = new ArrayList<FeatureSet>();
     private static Object[] thisParameter = new Object[0];
-    private static String[] allSeqIDs = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y"};
+    private static String[] allSeqIDs = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y",
+    	"chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY"};
 
     
     public static List<FeatureSet> convertBase64StrToFeatureSets(final String sourceSets) {
