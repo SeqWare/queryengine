2225c88b02854de9d601fa1ce88640ab56cd0d04
bcso <bcso@uwaterloo.ca>
2014-03-18 13:12:06 -0400

    see why no feature set elements are being recognized

--- seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperTest.java
+++ seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperTest.java
@@ -174,7 +174,8 @@ public class QueryVCFDumperTest {
         Stack<SGID> runMain = QueryVCFDumper.runMain(argList.toArray(new String[argList.size()]));
 
         Assert.assertTrue("should have 1 resulting feature set, had " + runMain.size(), runMain.size() == 2);
-        System.out.println("FeatureSet elements for Second Test.... " + SWQEFactory.getQueryInterface().getLatestAtomBySGID(runMain.pop(), FeatureSet.class).getCount() + " rowkey: " + originalSet.getRowKey().toString());
+        
+        System.out.println("FeatureSet elements for Second Test.... " + SWQEFactory.getQueryInterface().getLatestAtomBySGID(runMain.pop(), FeatureSet.class).getCount() + " rowkey: " + originalSet.getRowKey().toString() + " " + runMain.pop().getRowKey());
         Assert.assertTrue("starting feature set was incorrect", SWQEFactory.getQueryInterface().getLatestAtomBySGID(runMain.pop(), FeatureSet.class).getCount() == 173);
         long count = SWQEFactory.getQueryInterface().getLatestAtomBySGID(runMain.pop(), FeatureSet.class).getCount();
         Assert.assertTrue("second query was incorrect, should have 10, found " + count, count == 10);
