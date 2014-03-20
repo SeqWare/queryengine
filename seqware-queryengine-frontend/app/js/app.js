5ff8b48fb95191a3d0906b8dd35a8cd28e9cc516
Jonathon Ho <jho@OLM-LOANER07.local>
2014-03-20 14:59:38 -0400

    Changes to featureset downloads and added screens for ReadSets, References

--- seqware-queryengine-frontend/app/js/app.js
+++ seqware-queryengine-frontend/app/js/app.js
@@ -16,14 +16,13 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/variants', {templateUrl: 'partials/variants.html', controller: 'VariantCtrl'});
   $routeProvider.when('/plugin', {templateUrl: 'partials/plugin.html', controller: 'PluginCtrl'});
   $routeProvider.when('/query', {templateUrl: 'partials/query.html', controller: 'QueryCtrl'});
+  $routeProvider.when('/reads', {templateUrl: 'partials/reads.html', controller: 'ReadCtrl'});
+  $routeProvider.when('/references', {templateUrl: 'partials/references.html', controller: 'ReferenceCtrl'});
+  $routeProvider.when('/referencesets', {templateUrl: 'partials/referencesets.html', controller: 'ReferenceSetCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
-}])
-/*.config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-])*/;
+}]);
 
+// Application Config
 angular.module('queryengineApp').constant('APP_CONFIG', {
   'webservice_url':'http://10.0.20.188:8889/seqware-queryengine-webservice/api/'
 });
