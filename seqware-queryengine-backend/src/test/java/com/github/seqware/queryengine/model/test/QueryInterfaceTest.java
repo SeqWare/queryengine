9b13dc26c2f3d32cb31d358cec3321ce356b3712
bcso <bcso@uwaterloo.ca>
2014-03-24 15:02:33 -0400

    try compiling a query instead

--- seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/model/test/QueryInterfaceTest.java
+++ seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/model/test/QueryInterfaceTest.java
@@ -201,12 +201,14 @@ public class QueryInterfaceTest implements Benchmarking {
 
     /**
      * <p>testTypeQuery.</p>
+     * @throws RecognitionException 
      */
     @Test
-    public void testTypeQuery() {
+    public void testTypeQuery() throws RecognitionException {
         // get a FeatureSet from the back-end
-        QueryFuture<FeatureSet> future = SWQEFactory.getQueryInterface().getFeaturesByAttributes(0, aSet, new RPNStack(
-                new Constant("type1"), new FeatureAttribute("type"), Operation.EQUAL));
+//        QueryFuture<FeatureSet> future = SWQEFactory.getQueryInterface().getFeaturesByAttributes(0, aSet, new RPNStack(
+//                new Constant("type1"), new FeatureAttribute("type"), Operation.EQUAL));
+        QueryFuture<FeatureSet> future = SWQEFactory.getQueryInterface().getFeaturesByAttributes(0, aSet, RPNStack.compileQuery("type=type1"));
         // check that Features are present match
         FeatureSet result = future.get();
         for (Feature f : result) {
