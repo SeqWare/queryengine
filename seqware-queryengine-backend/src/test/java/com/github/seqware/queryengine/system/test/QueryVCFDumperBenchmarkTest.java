b9c122b1e8f0b543d0611ed43f1e8cb14b864beb
bcso <bcso@uwaterloo.ca>
2014-03-25 14:42:16 -0400

    Update todos

--- seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperBenchmarkTest.java
+++ seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperBenchmarkTest.java
@@ -70,7 +70,6 @@ public class QueryVCFDumperBenchmarkTest implements Benchmarking{
 	@BeforeClass
 	public static void setUpTest(){
 		try{
-			//TODO: Download File
 	        String vcf = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/analysis_results/consensus_call_sets/indels/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
 	        testingFiles = download(vcf);
 	//        testingFiles.add(new File("/home/seqware/gitroot/queryengine/seqware-queryengine-backend/src/test/resources/com/github/seqware/queryengine/system/FeatureImporter/consequences_annotated.vcf"));
