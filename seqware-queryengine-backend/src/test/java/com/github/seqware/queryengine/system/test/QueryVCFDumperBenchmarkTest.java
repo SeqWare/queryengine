691be53b1ed5122e7c04fb1ad3d8a1f5897871e7
bcso <bcso@uwaterloo.ca>
2014-03-27 09:31:00 -0400

    added a smaller file, and some comments

--- seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperBenchmarkTest.java
+++ seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperBenchmarkTest.java
@@ -65,8 +65,15 @@ public class QueryVCFDumperBenchmarkTest implements Benchmarking{
 	public static void setUpTest(){
 		try{
 			if (QUICK_TEST == false){
-				String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
-//		        String vcf = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/analysis_results/consensus_call_sets/indels/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
+				/**This file contains 1,000,000 lines**/
+				String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites1MillionLines.vcf.gz";
+				
+				/**This file contains 2,000,000 lines**/
+				//String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
+				
+				/**This file contains 4,000,000 lines**/
+		        //String vcf = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/analysis_results/consensus_call_sets/indels/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
+				
 		        testingFiles = download(vcf);
 		        FIRST_QUERY = "start>=61800882 && stop <=81800882";
 		        SECOND_QUERY = "start>=61800882 && stop <=81800882 && (seqid==\"X\" || seqid==\"19\")";
