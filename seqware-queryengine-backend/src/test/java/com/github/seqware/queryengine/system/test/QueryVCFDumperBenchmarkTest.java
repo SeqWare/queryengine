d14201365a62116158d386f40b2304575ff7e313
bcso <bcso@uwaterloo.ca>
2014-03-31 09:30:55 -0400

    use only naive overlaps for 2000000 lines

--- seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperBenchmarkTest.java
+++ seqware-queryengine-backend/src/test/java/com/github/seqware/queryengine/system/test/QueryVCFDumperBenchmarkTest.java
@@ -67,13 +67,13 @@ public class QueryVCFDumperBenchmarkTest implements Benchmarking{
 			if (QUICK_TEST == false){
 				
 				/**This file contains 500,000 lines**/
-				String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites500000Lines.vcf.gz";
+//				String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites500000Lines.vcf.gz";
 				
 				/**This file contains 1,000,000 lines**/
 //				String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites1MillionLines.vcf.gz";
 				
 				/**This file contains 2,000,000 lines**/
-//				String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
+				String vcf = "https://dl.dropboxusercontent.com/u/3238966/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
 				
 				/**This file contains 4,000,000 lines**/
 		        //String vcf = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/analysis_results/consensus_call_sets/indels/ALL.wgs.VQSR_V2_GLs_polarized_biallelic.20101123.indels.sites.vcf.gz";
@@ -102,7 +102,7 @@ public class QueryVCFDumperBenchmarkTest implements Benchmarking{
 		}
 	}
 	
-	@Test
+//	@Test
 	public void testBinning(){
 		try{
 			setOverlapStrategy(Constants.OVERLAP_STRATEGY.BINNING);
